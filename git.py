2dcbd41c7d3d7f42348290e6271c78a685bd995d
llawlaw23 <llaaww23@gmail.com>
2024-12-16 15:25:29 +0000

    updated README.md

--- git.py
+++ git.py
@@ -1,4 +1,8 @@
-Terms = ["clone", "add", "commit", "push", "pull"]
+Git_terms = ["cd", "dir", "ls"] 
+commands = ["clone", "add", "commit", "push", "pull" ]
 
-print(type(Terms))
-print(Terms[1])
+print(type(Git_terms))
+print(Git_terms[1])
+
+print(type(commands))
+print(commands[1])
